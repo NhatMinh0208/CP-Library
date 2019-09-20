bab7c145f09fd1e00e37f636451f5081a1cb9ed0
NhatMinh0208 <minhkhicon2468@gmail.com>
2019-09-20 21:20:00 +0700

    Add solution for problem `D - Make The Fence Great Again` Link: https://codeforces.com/contest/1221/problem/D Tags: dp, *1800 Ref: https://codeforces.com/contest/1221/submission/60932928

--- codeforces/1221/D.cpp
+++ codeforces/1221/D.cpp
@@ -1,15 +1,43 @@
-#include <iostream>
-#define ll long long
+// Credit to robs77 for this template
+/***********HEADER***************/
+#include <bits/stdc++.h>
 using namespace std;
+/***********MACROS***************/
+//#define int long long 
+#define ll int64_t 
+#define LL long long
+#define fri(l,k) for (i=l;i<=k;i++)
+#define frj(l,k) for (j=l;j>=k;j--)
+#define fij(a,b,l,k) for(i=a;i<=b;i++) for(j=l;j<=k;j++)
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+#define endl "\n"
+#define pb push_back
+#define _1 first
+#define _2 second
+#define mxsz 1000000007
+#define mxval 1e9 + 7
+#define grt [](auto x,auto y){return x>y;}
+#define pii pair<int,int>
+#define fi first 
+#define se second 
+ 
+/*********VARIABLE****************/
 struct bd {ll v; ll c;};
+typedef pair<int,int> ii;
 /******************************/
 const ll po[12]={1,2,4,8,16,32,64,128,256,512,1024,2048};
 ll n,i,j,x,y,z,minn=0,t,pr,a,b,k;
 bd all[300001]; bd h;
 ll dp[300001][3];
-int main()
-{
-    	ios::sync_with_stdio(0);
+/*********FUNCTIONS**************/
+ 
+ 
+/***********MAIN**************/
+ 
+signed main()
+{ 
+	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin>>t;
 	for (z=1;z<=t;z++) { x=1;
