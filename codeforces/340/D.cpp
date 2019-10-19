dc42173087262ce564c4e28a7efa12b6751f74d6
NhatMinh0208 <minhkhicon2468@gmail.com>
2019-10-19 22:27:00 +0700

    Add solution for problem `D - Bubble Sort Graph` Link: https://codeforces.com/contest/340/problem/D Tags: binary search, data structures, dp, *1500 Ref: https://codeforces.com/contest/340/submission/62940580

--- codeforces/340/D.cpp
+++ codeforces/340/D.cpp
@@ -1,19 +1,24 @@
 #include<bits/stdc++.h> 
 using namespace std;  
   #define int long long
-int lis(vector<int>& v)  
+int LongestIncreasingSubsequenceLength(std::vector<int>& v)  
 {  
     if (v.size() == 0)  
         return 0;  
   
     std::vector<int> tail(v.size(), 0);  
-    int length = 1; 
+    int length = 1; // always points empty slot in tail  
   
     tail[0] = v[0];  
       
     for (int i = 1; i < v.size(); i++) {  
+  
+            // Do binary search for the element in  
+            // the range from begin to begin + length 
         auto b = tail.begin(), e = tail.begin() + length; 
         auto it = lower_bound(b, e, v[i]);  
+              
+        // If not present change the tail element to v[i]  
         if (it == tail.begin() + length) 
         tail[length++] = v[i];  
         else   
@@ -24,13 +29,10 @@ int lis(vector<int>& v)
 }  
   
 signed main()  
-{  
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int n,i,j,m;
-    vector<int> v;
+{  int n,i,j,m;
+    std::vector<int> v;
 	cin>>n;
 	for (i=0;i<n;i++) {cin>>m; v.push_back(m);}  
-    cout << lis(v);  
+    std::cout << LongestIncreasingSubsequenceLength(v);  
+    return 0;  
 }   
