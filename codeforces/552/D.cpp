57892a678fde99aa3fd520557afba2d9eef0d2d6
NhatMinh0208 <minhkhicon2468@gmail.com>
2020-01-04 10:21:00 +0700

    Add solution for problem `D - Vanya and Triangles` Link: https://codeforces.com/contest/552/problem/D Tags: brute force, combinatorics, data structures, geometry, math, sortings, *1900 Ref: https://codeforces.com/contest/552/submission/68144747

--- codeforces/552/D.cpp
+++ codeforces/552/D.cpp
@@ -1,32 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
- #define map mapp
- #define endl "\n"
+ 
 const int mx=2005;
-int x[mx], y[mx], map[201][201], dx, dy, l, xx, yy;
+int x[mx], y[mx];
  
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 	int n; cin>>n;
-	for (int i=1;i<=n;i++) {cin>>x[i]>>y[i]; map[x[i]+100][y[i]+100]=1;}
-	long long res=0;
-	for (int i=1;i<n;i++)
-	for (int j=i+1;j<=n;j++)
-	{
-		res+=n+1;
-		dx=x[j]-x[i];
-		dy=y[j]-y[i];
-		l=__gcd(dx,dy);
-		dx/=l;
-		dy/=l;
-		xx=x[i];
-		yy=y[i];
-		while ((xx<=100)and(yy<=100)and(xx>=-100)and(yy>=-100)) {res-=map[xx+100][yy+100]; xx+=dx; yy+=dy;}
-		xx=x[i];
-		yy=y[i];
-		while ((xx<=100)and(yy<=100)and(xx>=-100)and(yy>=-100)) {res-=map[xx+100][yy+100]; xx-=dx; yy-=dy;}
-	}
-	cout<<res/3;
+	for (int i=0; i<n; i++) cin>>x[i]>>y[i];
+	int ret=0;
+	for (int i=0; i<n; i++)
+		for (int j=i+1; j<n; j++)
+			for (int k=j+1; k<n; k++)
+				if(x[i]*(y[j]-y[k])+x[j]*(y[k]-y[i])+x[k]*(y[i]-y[j])!=0) ret++;
+	cout << ret << endl;
 }
