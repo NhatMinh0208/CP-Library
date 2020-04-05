ed885d27fefcbb81a3dca4ccf023c641f8958a95
NhatMinh0208 <minhkhicon2468@gmail.com>
2020-04-05 17:13:00 +0700

    Add solution for problem `E - Game of Stones` Link: https://codeforces.com/contest/768/problem/E Tags: bitmasks, dp, games, *2100 Ref: https://codeforces.com/contest/768/submission/75556492

--- codeforces/768/E.cpp
+++ codeforces/768/E.cpp
@@ -1,58 +1,67 @@
-/*
-khoi orz, go check out his algo
--normie-
-*/
+//----------NMFF0B0H----------//
 #include <bits/stdc++.h>
-using namespace std;
-#define rep(i,n) for(int64_t i=0;i < (int64_t)(n);i++)
-#pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-#define FILE_IN "robot.inp"
-#define FILE_OUT "robot.out"
-#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
-#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define nfio cin.tie(0);cout.tie(0)
-#define max(x,y) (((x)>(y))?(x):(y))
-#define min(x,y) (((x)<(y))?(x):(y))
-#define ord(a,b,c) ((a>=b)and(b>=c))
-#define MOD (ll(1000000007))
+#define IM using
+#define GAY namespace
+#define LOL std;
+IM GAY LOL
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define rep(i, n) for (int64_t i = 0; i < (int64_t)(n); i++)
+#define max(a,b) (((a)>(b))?(a):(b))
+#define min(a,b) (((a)<(b))?(a):(b))
+#define FILE_IN "ballin.txt"
+#define FILE_OUT "ballout.txt"
+#define ofile                     \
+    freopen(FILE_IN, "r", stdin); \
+    freopen(FILE_OUT, "w", stdout)
+#define fio                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define nfio    \
+    cin.tie(0); \
+    cout.tie(0)
+#define ord(a, b, c) ((a >= b) and (b >= c))
+#define MOD (ll(18101203))
 #define MAX 300001
-#define mag 1048576
+#define mag 320
+#define p1 first
+#define p2 second.first
+#define p3 second.second
 #define fi first
 #define se second
-#define pow2(x) (ll(1)<<x)
-#define pii pair<int,int>
-#define piii pair<int,pii>
-#define pll pair<ll,ll>
-#define plll pair<ll,pll>
-#define For(i,__,___) for(int i=__;i<=___;i++)
-#define Rep(i,__,___) for(int i=__;i>=___;i--)
-#define ordered_set tree<long long,null_type,less<long long>,rb_tree_tag,tree_order_statistics_node_update>
+#define pow2(x) (ll(1) << x)
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define For(i, __, ___) for (int i = __; i <= ___; i++)
+#define Rep(i, __, ___) for (int i = __; i >= ___; i--)
+#define ordered_set tree<long long, null_type, less<long long>, rb_tree_tag, tree_order_statistics_node_update>
 #define endl "\n"
-#define bi BigInt
-#define ll long long
-#define pi 3.1415926535897
-//------START-----------//
-//------END-----------//
-ll n,m,k,t,t1,i,j,a,b, res=-1,resi,resj;
-vector<ll> v;
+#define vi vector<int>
+#define bi bigint
+#define get geet
+#define list_of_primes {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997}
+typedef long long ll;
+//--------END----------//
+int sg[100010];
 int main()
 {
-//	ofile;
-	fio;
-	cin>>t;
-	for (t1=0;t1<t;t1++)
-	{
-		cin>>b;
-		i=0;
-		while(b>=i)
-		{
-			b-=i;
-			i++;
+    fio;
+	int n;
+	int st=0;
+	cin>>n;
+	for(int i=1;i<=100;i++){
+		for(int j=1;j<=i+1;j++){
+			sg[++st]=i;
 		}
-		i--;
-		a^=i;
 	}
-	if (a) cout<<"NO"; else cout<<"YES";
+	int ans=0;
+	while(n--){
+		cin>>st;
+		ans^=sg[st];
+	}
+	if(ans==0)cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
 }
