2facf8abb484d8d6dd479170bac6f22f5300dacc
NhatMinh0208 <minhkhicon2468@gmail.com>
2020-05-05 09:35:00 +0700

    Add solution for problem `C - Sonya and Problem Wihtout a Legend` Link: https://codeforces.com/contest/713/problem/C Tags: dp, sortings, *2300 Ref: https://codeforces.com/contest/713/submission/79015835

--- codeforces/713/C.cpp
+++ codeforces/713/C.cpp
@@ -1,60 +1,64 @@
+/*
+ A Submission by $%U%$
+ at time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+*/
 #include <bits/stdc++.h>
 using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define rep(i,n) for(int64_t i=0;i < (int64_t)(n);i++)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define FILE_IN "tayto.inp"
+#define FILE_OUT "tayto.out"
+#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define nfio cin.tie(0);cout.tie(0)
+#define max(x,y) (((x)>(y))?(x):(y))
+#define min(x,y) (((x)<(y))?(x):(y))
+#define ord(a,b,c) ((a>=b)and(b>=c))
+#define MOD (ll(1000000007))
+#define MAX 300001
+#define mag 320
+#define p1 first
+#define p2 second.first
+#define p3 second.second
+#define fi first
+#define se second
+#define pow2(x) (ll(1)<<x)
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define For(i,__,___) for(int i=__;i<=___;i++)
+#define Rep(i,__,___) for(int i=__;i>=___;i--)
+#define ordered_set tree<long long,null_type,less<long long>,rb_tree_tag,tree_order_statistics_node_update>
+#define endl "\n"
+#define bi BigInt
 typedef long long ll;
-struct slope_func 
-{
-	public:
-	ll a,b;
-	priority_queue<ll> bp;
-	slope_func(ll aa, ll bb, vector<ll> v)
-	{
-		a=aa;
-		b=bb;
-		for (ll g : v) bp.push(g);
-	}
-	void slope_add (ll aa, ll bb, vector<ll> v)
-	{
-		a+=aa;
-		b+=bb;
-		for (ll g : v) bp.push(g);
-	}	
-	void slope_min()
-	{
-		while(a+bp.size()>0) bp.pop();
-	}
-};
-ll n,m,i,j,k;
-int main()
-{
-//	freopen("fractions.inp","r",stdin);
-//	freopen("fractions.out","w",stdout);
-	auto cur=slope_func(0,0,{});
-	cin>>n;
-	for (i=1;i<=n;i++)
-	{
-		cin>>m; m-=i;
-		cur.slope_min();
-		cur.slope_add(-1,m,{m,m});
-	}	
-	vector<ll> lis;
-	while(cur.bp.size())
-	{
-		lis.push_back(cur.bp.top());
-		cur.bp.pop();
-	}
-	m=cur.a;
-	for (i=lis.size()-1;i>=0;i--)
-	{
-		if (m==0) break;
-		if (i==lis.size()-1)
-		{
-			k=lis[i]*cur.a+cur.b;
-		}
-		else
-		{
-			k+=(lis[i]-lis[i+1])*m;
-		}
-		m++;
-	}
-	cout<<k<<endl;
+//---------END-------//
+int main() {
+    fio;
+  int n;
+  cin >> n;
+  ll res = 0;
+  multiset<int, greater<int>> s;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
+    x -= i;
+    if(not i) {
+      s.emplace(x);
+    } else {
+      if(*s.begin() > x) {
+        res += *s.begin() - x;
+        s.erase(s.begin());
+        s.emplace(x);
+        s.emplace(x);
+      } else {
+        s.emplace(x);
+      }
+    }    
+  }
+  cout << res;
 }
