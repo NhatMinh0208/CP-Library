9493c988f58261056e602270a65ec8ca8740ad37
NhatMinh0208 <minhkhicon2468@gmail.com>
2020-06-19 10:51:00 +0700

    Add solution for problem `C - Kalila and Dimna in the Logging Industry` Link: https://codeforces.com/contest/319/problem/C Tags: dp, geometry, *2100 Ref: https://codeforces.com/contest/319/submission/84287827

--- codeforces/319/C.cpp
+++ codeforces/319/C.cpp
@@ -1,146 +1,85 @@
-/*
-	Normie's Template v2.0
-*/
-
-// Standard library in one include.
 #include <bits/stdc++.h>
-using namespace std;
-
-// ordered_set library.
+#define IM using
+#define GAY namespace
+#define LOL std;
+IM GAY LOL
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-#define ordered_set tree<long long,null_type,less<long long>,rb_tree_tag,tree_order_statistics_node_update>
-
-// AtCoder library. (Comment out these two lines if you're not submitting in AtCoder.) (Or if you want to use it in other judges, run expander.py first.)
-//#include <atcoder/all>
-//using namespace atcoder;
-
-//Pragmas (Comment out these three lines if you're submitting in szkopul.)
-#pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-
-//File I/O.
-#define FILE_IN "cseq.inp"
-#define FILE_OUT "cseq.out"
-#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
-
-//Fast I/O.
-#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define nfio cin.tie(0);cout.tie(0)
-#define endl "\n"
-
-//Order checking.
-#define ord(a,b,c) ((a>=b)and(b>=c))
-
-//Constants.
+#define rep(i, n) for (int64_t i = 0; i < (int64_t)(n); i++)
+#define FILE_IN "polygon.inp"
+#define FILE_OUT "polygon.out"
+#define ofile                     \
+    freopen(FILE_IN, "r", stdin); \
+    freopen(FILE_OUT, "w", stdout)
+#define fio                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define nfio    \
+    cin.tie(0); \
+    cout.tie(0)
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+#define ord(a, b, c) ((a >= b) and (b >= c))
 #define MOD (ll(1000000007))
 #define MAX 300001
 #define mag 320
-
-//Pairs and 3-pairs.
 #define p1 first
 #define p2 second.first
 #define p3 second.second
 #define fi first
 #define se second
-#define pii(element_type) pair<element_type,element_type>
-#define piii(element_type) pair<element_type,pii(element_type)>
-
-//Quick power of 2.
-#define pow2(x) (ll(1)<<x)
-
-//Short for-loops.
-#define For(i,__,___) for(int i=__;i<=___;i++)
-#define Rep(i,__,___) for(int i=__;i>=___;i--)
-
-//Typedefs.
+#define pow2(x) (ll(1) << x)
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define For(i, __, ___) for (int i = __; i <= ___; i++)
+#define Rep(i, __, ___) for (int i = __; i >= ___; i--)
+#define ordered_set tree<long long, null_type, less<long long>, rb_tree_tag, tree_order_statistics_node_update>
+#define endl "\n"
+#define vi vector<int>
 #define bi BigInt
+#define list_of_primes {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997}
 typedef long long ll;
-
-//---------END-------//
-struct node
-{
-	node *nl=0, *nr=0;
-	ll dom=0;
-};
-struct dynamic_li_chao
-{
-	vector<pii(ll)> func={{0,3e18+7}};
-	ll calc (ll cur, ll x)
-	{
-		return (func[cur].fi)*x+(func[cur].se);
-	}
-	node start;
-	void add_line(ll a, ll b)
-	{
-		func.emplace_back(a,b);
-		update(-1e9,1e9+1,&start,func.size()-1);
-	}
-	void update(ll l, ll r, node* cur, ll diff )
-	{
-		if (cur->dom==0)
-		{
-			cur->dom=diff;
-		}
-		else if (l+1==r) 
-		{
-			if (calc(cur->dom,l)>calc(diff,l))
-			cur->dom=diff;
-		}
-		else
-		{
-			ll mid=(l+r)/2;
-			ll doml=(calc(cur->dom,l)>calc(diff,l));
-			ll domm=(calc(cur->dom,mid)>calc(diff,mid));
-			if (domm)
-			{
-				ll z=cur->dom;
-				cur->dom=diff;
-				diff=z;
-			}
-			if (doml==domm)
-			{
-				if (!cur->nr) cur->nr=new(node);
-				update(mid,r,cur->nr,diff);
-			}
-			else
-			{
-				if (!cur->nl) cur->nl=new(node);
-				update(l,mid,cur->nl,diff);
-			}
-		}
-	}
-	ll get(ll x)
-	{
-		return getK(-1e9,1e9+1,&start,x);
-	}
-	ll getK(ll l, ll r, node* cur, ll tar)
-	{
-		ll res1=calc(cur->dom,tar);
-		if (l+1==r) return res1;
-		ll res2=3e18+7;
-		ll mid=(l+r)/2;
-		if ((tar<=mid)and(cur->nl)) res2=getK(l,mid,cur->nl,tar); 
-		else if ((tar>mid)and(cur->nr)) res2=getK(mid,r,cur->nr,tar);
-		return min(res1,res2);
-	}
-};
-dynamic_li_chao bruh;
-ll n,m,i,j,k,aa[100001],bb[100001];
-ll dp[100001];
-int main()
-{
-	fio;
-	cin>>n;
-	for (i=1;i<=n;i++) cin>>aa[i];
-	for (i=1;i<=n;i++) cin>>bb[i];
-	dp[1]=0;
-	for (i=2;i<=n;i++)
-	{
-		bruh.add_line(bb[i-1],dp[i-1]);
-		dp[i]=bruh.get(aa[i]);
-	}
-	cout<<dp[n];
+ 
+ 
+int const N = 100 * 1000 + 16;
+int const M = 1000*1000*1000 + 7;
+ 
+int n;
+ll a[N];
+ll b[N];
+ll dp[N];
+ 
+int main() {
+  cin.tie(0);
+  cin.sync_with_stdio(0);
+ 
+  cin >> n;
+  for (int i = 0; i < n; ++i)
+    cin >> a[i];
+ 
+  for (int i = 0; i < n; ++i)
+    cin >> b[i];
+    
+  auto f = [] (int l, int r, int k) {
+    return 1.0 * (dp[l]-dp[r]) / (b[l]-b[r]) <= 1.0 * (dp[l]-dp[k]) / (b[l]-b[k]);
+  };
+  
+  auto f2 = [] (int l, int r, int i) {
+    return b[l]*a[i]+dp[l] > b[r]*a[i]+dp[r];
+  };
+  
+  deque<int> d = {0};
+  for(int i = 1; i < n; ++i) {
+    while(d.size() > 1 && f2(d[0], d[1], i))
+      d.pop_front();
+ 
+    dp[i] = 1ll * a[i] * b[d.front()] + dp[d.front()];
+    while(d.size() > 1 && f(d[d.size()-2], d[d.size()-1], i))
+      d.pop_back();
+ 
+    d.emplace_back(i);
+  }
+  cout << dp[n-1];
 }
