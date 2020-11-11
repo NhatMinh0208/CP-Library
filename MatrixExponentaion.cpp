4f427521a54a18515967802d153305fc17287621
NhatMinh0208 <minhkhicon2468@gmail.com>
2020-11-11 02:19:41 +0000

    Updated to handle matrices of arbitary size

--- MatrixExponentaion.cpp
+++ MatrixExponentaion.cpp
@@ -1,107 +1,42 @@
-
-// Problem : E. Darth Vader and Tree
-// Contest : Codeforces - Codeforces Round #291 (Div. 2)
-// URL : https://codeforces.com/problemset/problem/514/E
-// Memory Limit : 256 MB
-// Time Limit : 2000 ms
-// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
-
-/*
- A Submission by $%U%$
- at time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
-*/
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define rep(i,n) for(int64_t i=0;i < (int64_t)(n);i++)
-#pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-#define FILE_IN "cseq.inp"
-#define FILE_OUT "cseq.out"
-#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
-#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define nfio cin.tie(0);cout.tie(0)
-#define max(x,y) (((x)>(y))?(x):(y))
-#define min(x,y) (((x)<(y))?(x):(y))
-#define ord(a,b,c) ((a>=b)and(b>=c))
-#define MOD (ll(1000000007))
-#define MAX 300001
-#define mag 320
-#define p1 first
-#define p2 second.first
-#define p3 second.second
+typedef long long ll;
+#define pll pair<ll,ll>
 #define fi first
 #define se second
-#define pow2(x) (ll(1)<<x)
-#define pii pair<int,int>
-#define piii pair<int,pii>
-#define For(i,__,___) for(int i=__;i<=___;i++)
-#define Rep(i,__,___) for(int i=__;i>=___;i--)
-#define ordered_set tree<long long,null_type,less<long long>,rb_tree_tag,tree_order_statistics_node_update>
-#define endl "\n"
-#define bi BigInt
-typedef long long ll;
-//---------END-------//
-vector<vector<ll>> trans(102,vector<ll>(102));
-vector<ll> base(102);
-vector<ll> vm_mul(vector<ll> a, vector<vector<ll>> b)
+int n,m,i,j,k,t,u,v,a,b; ll x,t1;
+int arr[1000001],pre[1000001],las[1000001];
+vector<vector<ll>> mulmm(vector<vector<ll>> a, vector<vector<ll>> b)
 {
-	ll i,j;
-	vector<ll> c(102);
-	for (i=0;i<=101;i++) for (j=0;j<=101;j++)
+	ll n=a.size();
+//	cout<<a.size()<<a[0].size()<<b.size()<<b[0].size()<<endl;
+	vector<vector<ll>> c=vector<vector<ll>>(n,vector<ll>(n,0));
+//	cout<<c.size()<<c[0].size()<<endl;
+	for (ll i=0;i<n;i++) for (ll j=0;j<n;j++) for (ll u=0;u<n;u++)
+	{
+//		cout<<i<<' '<<j<<' '<<u<<endl;
+		c[i][j]+=a[i][u]*b[u][j];
+		c[i][j]%=k;
+	}
+	return c;
+}
+vector<ll> mulvm (vector<ll> a, vector<vector<ll>> b)
+{
+	ll n=a.size();
+	vector<ll> c=vector<ll> (n,0);
+	for (ll i=0;i<n;i++) for (ll j=0;j<n;j++) 
 	{
 		c[j]+=a[i]*b[i][j];
-		c[j]%=MOD;
+		c[j]%=k;
 	}
 	return c;
 }
-vector<vector<ll>> mm_mul(vector<vector<ll>> a, vector<vector<ll>> b)
+vector<vector<ll>> bowm(vector<vector<ll>> a, ll x)
 {
-	ll i,j,k;
-	vector<vector<ll>> c=a;
-	for (i=0;i<=101;i++) for (j=0;j<=101;j++)
-	{
-		c[i][j]=0;
-	}
-	for (i=0;i<=101;i++) for (j=0;j<=101;j++) for (k=0;k<=101;k++)
-	{
-		c[i][k]+=a[i][j]*b[j][k];
-		c[i][k]%=MOD;
-	}
-	return c;
-}
-vector<vector<ll>> bow(vector<vector<ll>> a, ll x)
-{
+//	cout<<"bowm "<<' '<<x<<endl;
 	if (x==1) return a;
-	vector<vector<ll>> res=bow(a,x/2);
-	res=mm_mul(res,res);
-	if (x%2) res=mm_mul(res,a);
+	auto res=bowm(a,x/2);
+	res=mulmm(res,res);
+	if (x%2) res=mulmm(res,a);
 	return res;
 }
-ll n,m,k,c[501],pos[100001],t,t1,i,j;
-int main()
-{
-//  ofile;
-    fio;
-    cin>>n>>k;
-    if (k==0) 
-    {
-    	cout<<1; return 0;
-    }
-    for (i=0;i<101;i++) trans[i][i+1]=1;
-    trans[101][101]=1;
-    for (i=1;i<=n;i++)
-    {
-    	cin>>m;
-    	trans[m-1][0]++;
-    }
-    base[0]=1;
-    base=vm_mul(base,bow(trans,k));
-    t=0;
-    for (ll g:base) t+=g;
-    cout<<t%MOD;
-}
-
