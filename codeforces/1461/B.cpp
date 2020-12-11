3eba8b385e6d4759435b7792ce3d23cd36a724f2
NhatMinh0208 <minhkhicon2468@gmail.com>
2020-12-11 21:44:00 +0700

    Add solution for problem `B - Find the Spruce` Link: https://codeforces.com/contest/1461/problem/B Tags: brute force, dp, implementation, *1400 Ref: https://codeforces.com/contest/1461/submission/100918835

---
+++ codeforces/1461/B.cpp
@@ -0,0 +1,106 @@
+/*
+        Normie's Template v2.0
+*/
+
+// Standard library in one include.
+#include <bits/stdc++.h>
+using namespace std;
+
+// ordered_set library.
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set(el) tree<el,null_type,less<el>,rb_tree_tag,tree_order_statistics_node_update>
+
+// AtCoder library. (Comment out these two lines if you're not submitting in AtCoder.) (Or if you want to use it in other judges, run expander.py first.)
+//#include <atcoder/all>
+//using namespace atcoder;
+
+//Pragmas (Comment out these three lines if you're submitting in szkopul.)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+//File I/O.
+#define FILE_IN "cseq.inp"
+#define FILE_OUT "cseq.out"
+#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
+
+//Fast I/O.
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define nfio cin.tie(0);cout.tie(0)
+#define endl "\n"
+
+//Order checking.
+#define ord(a,b,c) ((a>=b)and(b>=c))
+
+//min/max redefines, so i dont have to resolve annoying compile errors.
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+
+//Constants.
+#define MOD (ll(998244353))
+#define MAX 300001
+#define mag 320
+
+//Pairs and 3-pairs.
+#define p1 first
+#define p2 second.first
+#define p3 second.second
+#define fi first
+#define se second
+#define pii(element_type) pair<element_type,element_type>
+#define piii(element_type) pair<element_type,pii(element_type)>
+
+//Quick power of 2.
+#define pow2(x) (ll(1)<<x)
+
+//Short for-loops.
+#define ff(i,__,___) for(int i=__;i<=___;i++)
+#define rr(i,__,___) for(int i=__;i>=___;i--)
+
+//Typedefs.
+#define bi BigInt
+typedef long long ll;
+typedef long double ld;
+typedef short sh;
+//---------END-------//
+ll dp1[501][501],dp2[501][501];
+ll n,m,i,j,k,t,t1,u,v,a,b;
+ll mp[501][501];
+int main()
+{
+	fio;
+	cin>>t;
+	for (t1=0;t1<t;t1++)
+	{
+		cin>>n>>m;
+		u=0;
+		for (i=1;i<=n;i++)
+		for (j=1;j<=m;j++)
+		{
+		char c;
+		cin>>c;
+		mp[i][j]=(c=='*');
+		}
+		for (i=1;i<=n;i++)
+		{
+		for (j=1;j<=m;j++)
+		{
+			if (!mp[i][j])
+			dp1[i][j]=0;
+			else if ((i==1)or(j==1)) dp1[i][j]=1;
+			else dp1[i][j]=min(dp1[i-1][j],dp1[i][j-1])+1;
+		}
+		for (j=m;j>=1;j--)
+		{
+			if (!mp[i][j])
+			dp2[i][j]=0;
+			else if ((i==1)or(j==m)) dp2[i][j]=1;
+			else dp2[i][j]=min(dp2[i-1][j],dp2[i][j+1])+1;
+		}
+		for (j=1;j<=m;j++) u+=min(dp1[i][j],dp2[i][j]);
+		}
+		cout<<u<<endl;
+	}
+}
