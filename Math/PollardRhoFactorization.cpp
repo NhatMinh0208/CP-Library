8cdfa3ba447dc057f117c1d4362043cd595f46ba
Theoth Normie <minhkhicon2468@gmail.com>
2024-03-19 21:54:11 +0800

    Update PollardRhoFactorization.cpp

--- Math/PollardRhoFactorization.cpp
+++ Math/PollardRhoFactorization.cpp
@@ -1,56 +1,117 @@
+#ifndef CPL_TEMPLATE
+#define CPL_TEMPLATE
 /*
-Pollard - Rho factorization algorithm.
-Tested on library-checker.
+	Normie's Template v2.6
+	Changes:
+	Added range
 */
+// Standard library in one include.
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define rep(i,n) for(int64_t i=0;i < (int64_t)(n);i++)
-#pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-#define FILE_IN "birds.inp"
-#define FILE_OUT "birds.out"
+ 
+// ordered_set library.
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// #define ordered_set(el) tree<el,null_type,less<el>,rb_tree_tag,tree_order_statistics_node_update>
+ 
+// AtCoder library. (Comment out these two lines if you're not submitting in AtCoder.) (Or if you want to use it in other judges, run expander.py first.)
+//#include <atcoder/all>
+//using namespace atcoder;
+
+//Pragmas (Comment out these three lines if you're submitting in szkopul or USACO.)
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast,unroll-loops,tree-vectorize")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+ 
+//File I/O.
+#define FILE_IN "cseq.inp"
+#define FILE_OUT "cseq.out"
 #define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
-#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define nfio cin.tie(0);cout.tie(0)
-#define max(x,y) (((x)>(y))?(x):(y))
-#define min(x,y) (((x)<(y))?(x):(y))
+ 
+//Fast I/O.
+#define fio ios::sync_with_stdio(0);cin.tie(0)
+#define nfio cin.tie(0)
+#define endl "\n"
+ 
+//Order checking.
 #define ord(a,b,c) ((a>=b)and(b>=c))
-#define MOD (ll(1000000007))
+ 
+//min/max redefines, so i dont have to resolve annoying compile errors.
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+
+// Fast min/max assigns to use with AVX.
+// Requires g++ 9.2.0.
+// template<typename T>
+// __attribute__((always_inline)) void chkmin(T& a, const T& b) {
+//     a=(a<b)?a:b;
+// }
+
+// template<typename T>
+// __attribute__((always_inline)) void chkmax(T& a, const T& b) {
+//     a=(a>b)?a:b;
+// }
+ 
+//Constants.
+#define MOD (ll(998244353))
 #define MAX 300001
 #define mag 320
+const long double PI=3.14159265358979;
+ 
+//Pairs and 3-pairs.
 #define p1 first
 #define p2 second.first
 #define p3 second.second
 #define fi first
 #define se second
+#define pii(element_type) pair<element_type,element_type>
+#define piii(element_type) pair<element_type,pii(element_type)>
+
+//Custom begin/end shorthand.
+#define rnge(rnge) rnge.begin(),rnge.end()
+ 
+//Quick power of 2.
 #define pow2(x) (ll(1)<<x)
-#define pii pair<int,int>
-#define piii pair<int,pii>
-#define For(i,__,___) for(int i=__;i<=___;i++)
-#define Rep(i,__,___) for(int i=__;i>=___;i--)
-#define ordered_set tree<long long,null_type,less<long long>,rb_tree_tag,tree_order_statistics_node_update>
-#define endl "\n"
+ 
+//Short for-loops.
+#define ff(i,__,___) for(int i=__;i<=___;i++)
+#define rr(i,__,___) for(int i=__;i>=___;i--)
+ 
+//Typedefs.
 #define bi BigInt
-#define ll128 __int128
-#define pi 3.1415926535897
-//------START-----------//
+typedef long long ll;
+typedef long double ld;
+typedef short sh;
+
+// Binpow and stuff
+ll BOW(ll a, ll x, ll p)
+{
+	if (!x) return 1;
+	ll res=BOW(a,x/2,p);
+	res*=res;
+	res%=p;
+	if (x%2) res*=a;
+	return res%p;
+}
+ll INV(ll a, ll p)
+{
+	return BOW(a,p-2,p);
+}
+//---------END-------//
+#endif
+
 //	Random number generator. Works by defining a generator as below:
 //	generator gen(lower_bound,upper_bound)
 //	and then call gen(rng).
     std::random_device dev;
     std::mt19937 rng(dev());
  #define generator std::uniform_int_distribution<std::mt19937::result_type> 
-//---------END---------//
-typedef long long ll;
 
-ll mul64(ll a, ll b, ll mo)
 // Function to multiply 2 long longs using binary exponentation and then return the result mod mo.
 // Input: a,b,mo
 // Output: (a*b)%mo
+ll mul64(ll a, ll b, ll mo)
 {
     ll res = 0;
     while (b) {
@@ -62,27 +123,9 @@ ll mul64(ll a, ll b, ll mo)
     return res;
 }
 
-ll mul(ll a, ll b, ll mo)
-// Function to multiply 2 long longs using binary exponentation and then return the result mod mo.
-// Input: a,b,mo
-// Output: (a*b)%mo
-{
-	return (a*b)%mo;
-}
-ll bow (ll a, ll x, ll mo)
 // Input: a,x,mo
 // Output: (a^x)%mo
-{
-	if (x==0) return 1;
-	ll res=bow(a,x/2,mo);
-	res=mul(res,res,mo);
-	if (x%2) res=mul(res,a,mo);
-	return res;
-}
-
 ll bow64 (ll a, ll x, ll mo)
-// Input: a,x,mo
-// Output: (a^x)%mo
 {
 	if (x==0) return 1;
 	ll res=bow64(a,x/2,mo);
@@ -90,8 +133,9 @@ ll bow64 (ll a, ll x, ll mo)
 	if (x%2) res=mul64(res,a,mo);
 	return res;
 }
-ll check (ll n, ll a, ll po, ll re)
+
 // Checks if (a^n-1) is divisible by n using Miller-Rabin optimization
+ll check (ll n, ll a, ll po, ll re)
 {
 	ll og=bow64(a,re,n);
 	if (og==1) return true;
@@ -103,8 +147,9 @@ ll check (ll n, ll a, ll po, ll re)
 	}
 	return false;
 }
-ll isPrime(ll n)
+
 // Does a deterministic Miller-Rabin primality test for n<=2^63
+ll isPrime(ll n)
 {
 	ll k=n-1,po=0,re=1;
 	while (k%2==0)
@@ -119,49 +164,48 @@ ll isPrime(ll n)
 	if (!check(n,i,po,re)) return false;
 	return true;
 }
+
+// The polynomial function (x^2+bx+c)%mo.
 ll f(ll x, ll y, ll z, ll mo)
-// The polynomial function (x^2+bx+c)%mo. Weirdly enough, the function (x^2+c)%mo gives TLE on test "4" for some reason.
 {
 	return ((mul64(x,x,mo)+mul64(x,y,mo))%mo+z)%mo;
 }
-ll gcd (ll x1, ll x2)
+
 // GCD function for long long.
+ll gcd (ll x1, ll x2)
 {
 	if (x1*x2==0) return max(x1,x2);
 	return gcd(min(x1,x2),max(x1,x2)%min(x1,x2));
 }
-ll pollard (ll n)
+
 // Returns one factor of n (can be n), or -1 if the number is prime.
+// Idea: use a function x -> (x^2+bx+c)%mo. We should get into a cycle after ~O(sqrt(factor_of_n)) steps.
+ll pollard (ll n)
 {
-//	cout<<"pollard("<<n<<")\n";
 	if (isPrime(n)) return -1;
 	generator gen(2,1e9),gen2(2,1e9);
 	ll start=gen(rng); ll seed=gen(rng),seed2=gen(rng); // Starting number and seeds for function
-//	cout<<"start="<<start<<endl;
-//	cout<<"seed="<<seed<<endl;
 	ll x1=start,x2=start;
 	while(true)
 	{
 		// Does Floyd's cycle-finding algorithm.
-//		cout<<x1<<' '<<x2<<endl;
+		// The algorithm terminates when x1%fac == x2%fac for some factor of n.
 		x1=f(x1,seed,seed2,n);
 		x2=f(x2,seed,seed2,n);
 		x2=f(x2,seed,seed2,n);
-//		cout<<x1<<' '<<x2<<endl;
-		if (gcd(abs(x1-x2),n)>1) // found the cycle! Reuturns the factor.
+		if (gcd(abs(x1-x2),n)>1) // found the cycle - return the factor
 		{
-//	cout<<"end_pollard"<<endl;
 			return gcd(abs(x1-x2),n);
 		}
 	}
 }
-vector<ll> factorize(ll n) //Factorizes n. Return a list of prime factors.
+// Factorizes n. Return a list of prime factors.
+vector<ll> factorize(ll n) 
 {
-//	cout<<n<<endl;
+	if (n<=1) return {};
 	while(true)
 	{
 		ll g=pollard(n);
-//		cout<<g<<' ';
 		if (g==-1) return {n}; // Base case: n is a prime
 		else if ((g<n)and(g>1)) // Make sure that we find an actual factor
 		{
